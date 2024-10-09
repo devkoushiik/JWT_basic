9e85d7792eb119351f8b1c50d239d34f49fa5681
koushikxahmed <koushikxahmed@gmail.com>
2024-10-09 16:11:11 +0600

    Basic JWT Preparation done

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+const CustomAPIError = require('./custom-error')
+const BadRequestError = require('./bad-request')
+const UnAuthenticatedError = require('./unAuthenticated-error')
+
+module.exports = {CustomAPIError, BadRequestError, UnAuthenticatedError};
